df32f7646ed82b98036445345a37049749c16d07
minataour <1njust1c104@gmail.com>
2024-01-25 16:02:40 +0530

    Header dom

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA56GI1aFZt_VMN5onZtUwYTzwsw2A6m5Y",
+    authDomain: "project-94a17.firebaseapp.com",
+    projectId: "project-94a17",
+    storageBucket: "project-94a17.appspot.com",
+    messagingSenderId: "241430815599",
+    appId: "1:241430815599:web:61354b9163d014a9a3be9a",
+    measurementId: "G-1G5C2Z1VWX"
+  };
