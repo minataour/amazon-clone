a82e978b0f8b06a0a2e8226591cff7fd2b49e159
minataour <1njust1c104@gmail.com>
2024-01-27 13:54:35 +0530

    Setting cart states through reducer

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import './Header.css'
 import SearchIcons from "@material-ui/icons/Search"
 import ShoppingBasketIcons from "@material-ui/icons/ShoppingBasket"
 import {Link} from "react-router-dom"
+import { useStateValue } from './StateProvider'
 
 function Header() {
+    const [{basket}, dispatch] = useStateValue()
   return (
     <div className='header'>
         <Link to="/">
@@ -44,7 +46,7 @@ function Header() {
             <Link to="/checkout">
                 <div className='header__optionBasket'>
                     <ShoppingBasketIcons />
-                    <span className='header__optionLineTwo header__basketCount'>0</span>
+                    <span className='header__optionLineTwo header__basketCount'>{basket?.length}</span>
                 </div>
             </Link>
         </div>
