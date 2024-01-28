1cc0a83b0a6c42838de958c9c531b9c996b30989
minataour <1njust1c104@gmail.com>
2024-01-28 15:00:32 +0530

    Latest commands of firebase import and syntax. UPDATED!!

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {initializeApp} from "firebase/app"
+import {getAuth} from 'firebase/auth'
+import {getFirestore} from "firebase/firestore"
+
 const firebaseConfig = {
     apiKey: "AIzaSyA56GI1aFZt_VMN5onZtUwYTzwsw2A6m5Y",
     authDomain: "project-94a17.firebaseapp.com",
@@ -8,3 +12,10 @@ const firebaseConfig = {
     appId: "1:241430815599:web:61354b9163d014a9a3be9a",
     measurementId: "G-1G5C2Z1VWX"
   };
+
+  const firebaseApp = initializeApp(firebaseConfig)
+
+  const db = getFirestore(firebaseApp)
+  const auth = getAuth(firebaseApp)
+
+  export {db , auth}
