94e34592d132969cc5dd2f5145879c4acd2d8ca1
minataour <1njust1c104@gmail.com>
2024-01-29 01:41:40 +0530

    Signout and users able to submit. Firebase correctly setup in root folder and hosted on both domains.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import React from 'react'
 import './App.css';
 import Header from'./Header'
 import Home from './Home'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from './Checkout'
 import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 
 function App() {
+  const [{}, dispatch] = useStateValue()
+  
+  React.useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('the user is >>>', authUser)
+        if(authUser){
+          dispatch({
+            type: 'SET_USER',
+            user: authUser
+          })
+        } else {
+          dispatch({
+            type: 'SET_USER',
+            user: null
+          })
+        }
+    })
+  }, [])
+
   return (
     //BEM
     <Router>
--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import SearchIcons from "@material-ui/icons/Search"
 import ShoppingBasketIcons from "@material-ui/icons/ShoppingBasket"
 import {Link} from "react-router-dom"
 import { useStateValue } from './StateProvider'
+import { auth } from './firebase'
 
 function Header() {
-    const [{basket}, dispatch] = useStateValue()
+    const [{basket, user}, dispatch] = useStateValue()
+
+    const handleAuthentication = () => {
+        if(user) {
+            auth.signOut()
+        }
+    }
   return (
     <div className='header'>
         <Link to="/">
@@ -26,11 +33,11 @@ function Header() {
         </div>
 
         <div className="header__nav" >
-            <Link to='/login'>
-                <div className="header__option" >
+            <Link to={!user && '/login'}>
+                <div onClick={handleAuthentication} className="header__option" >
                     <span className="header__optionLineOne" >Hello Sign In</span>
                     <span 
-                    className="header__optionLineTwo" >Account</span>
+                    className="header__optionLineTwo" >{user ? 'Sign Out' : 'Account'}</span>
                 </div>
             </Link>
             
